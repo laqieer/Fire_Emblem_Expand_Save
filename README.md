4eb03faaad4f652d3b08b16ca135a9ffad454403
laqieer <laqieer@126.com>
2019-01-11 00:30:47 +0800

    solution: hack WriteSRAMFast function

--- README.md
+++ README.md
@@ -6,8 +6,4 @@ Fire Emblem on GBA use 32KB save.
 * FE6 & FE7: Sram_F_v102
 * FE8 : Sram_F_v103
 
-This project is to expand it to 64KB (FLASH_V123).
-
-## Dependency
-
-AGB Game Pak Backup Library
+This project is to expand it to 64KB (FLASH_Vnnn).
--- include/SRAM_Flash.h
+++ include/SRAM_Flash.h
@@ -4,21 +4,12 @@
 
 #pragma once
 
-#include "AgbFlash.h"
-#include <stddef.h>
-
-char flash_sector_buffer[4096];
-void (**gFlashTimerIntrFunc)(void) = 0;
-void *memcpy(void *str1, const void *str2, size_t n);
+extern volatile unsigned short GamePakWaitstateControl;
+extern volatile unsigned char FLASH_BASE_HI;
+extern volatile unsigned char FLASH_BASE_LO;
 
 #pragma long_calls
-void SetSramFastFunc_Flash_Core();
-void ReadSramFast_Flash_Core(unsigned char *src, unsigned char *dst, unsigned int size);
 void WriteSramFast_Flash_Core(unsigned char *src, unsigned char *dst, unsigned int size);
-unsigned int VerifySramFast_Flash_Core(unsigned char *src,unsigned char *tgt,unsigned int size);
 #pragma long_calls_off
 
-void SetSramFastFunc_Flash();
-void ReadSramFast_Flash(unsigned char *src, unsigned char *dst, unsigned int size);
 void WriteSramFast_Flash(unsigned char *src, unsigned char *dst, unsigned int size);
-unsigned int VerifySramFast_Flash(unsigned char *src,unsigned char *tgt,unsigned int size);
