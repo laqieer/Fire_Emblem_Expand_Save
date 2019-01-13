28d5b07a1b6ebb8fb52b62a5ee11c4072dfa1066
laqieer <laqieer@126.com>
2019-01-13 21:36:29 +0800

    Bugfix: memcpy return type
    it returns a pointer to destination

--- src/gba_memcpy.c
+++ src/gba_memcpy.c
@@ -3,7 +3,9 @@
 void *gba_memcpy(void *dest, void *src, unsigned int count)
 {
 	if(count && (count & 31 == 0) && ((u32)src & 3 == 0) && ((u32)dest & 3 == 0))
+	{
 		CpuFastCopy(src, dest, count);
-	else
-		memcpy(dest, src, count);
+		return dest;
+	}
+	return memcpy(dest, src, count);
 }
